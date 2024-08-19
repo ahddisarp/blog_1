ccc28dcb1124f8efac1787df652d932da6d6f643
Prasiddha Bhantana <pbhantan@myune.edu.au>
2024-08-19 22:57:20 +1000

    Created Blade views for admin panel and added roles with role-based access control.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -42,4 +43,17 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+    public function isAdmin()
+{
+    return $this->role === 'admin';
+}
+
+public function isAuthor()
+{
+    return $this->role === 'author';
+}
 }
