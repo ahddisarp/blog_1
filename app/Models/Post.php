11b51e442cef156816e63ddb6b2ccf91765b49bc
ahddisarp <ahddisarp.sh@gmail.com>
2024-09-30 12:00:42 +0545

    final commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
+
 
 class Post extends Model
 {
@@ -12,4 +15,9 @@ class Post extends Model
         'title',
         'content',
     ];
+    
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
 }
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,12 +3,13 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\Post;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
 class PostFactory extends Factory
 {
+    protected $model = Post::class;
     /**
      * Define the model's default state.
      *
